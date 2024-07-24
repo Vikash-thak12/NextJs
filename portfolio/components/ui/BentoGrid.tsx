c74502d9ee6a3df7d9ea38ee036722fa3a1c633e
Vikash Thakur <thakvikash622@gmail.com>
2024-07-24 21:06:32 +0530

    Imported Globe in card

--- portfolio/components/ui/BentoGrid.tsx
+++ portfolio/components/ui/BentoGrid.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { Globe } from "./Globe";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -93,6 +95,9 @@ export const BentoGridItem = ({
             {title}
           </div>
         </div>
+        {
+          id === 2 && <GlobeDemo />
+        }
       </div>
     </div >
   );
