fd116b4ff749945d05b8985d15657782e1a24ced
Vikash Thakur <thakvikash622@gmail.com>
2024-07-24 12:10:58 +0530

    almost complete

--- basic-project/src/app/page.tsx
+++ basic-project/src/app/page.tsx
@@ -1,31 +1,35 @@
 "use client"
 import { FormEvent, useState } from "react"
+import { useRouter } from 'next/navigation'
+// import { useRouter } from "next/router";
 
 export default function Home() {
-    const [inputval, setInputval] = useState('');
-    // console.log(inputval);
+  const [inputval, setInputval] = useState('');
+  const { push } = useRouter();
+  // console.log(inputval);
 
-    const handlesubmit = (e: FormEvent) => {
-        e.preventDefault();
-        console.log(inputval);
-        setInputval("")
-    }
+  const handlesubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(inputval);
+    setInputval("")
+    push(`predection/${inputval}`)
+  }
 
-  
 
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <div>
-                <h1 className="text-3xl mb-5">Enter your Name</h1>
-            </div>
-            <form onSubmit={handlesubmit} className="flex flex-col gap-5">
-                <input
-                className="px-4 py-2 rounded-lg bg-black"
-                    value={inputval}
-                    onChange={e => setInputval(e.target.value)}
-                    type="text" placeholder="Enter your name" />
-                <button className="bg-gray-600 px-4 py-2 rounded-md" type="submit">Submit</button>
-            </form>
-        </div>
-    )
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="bg-gray-600 p-4 rounded-2xl">
+        <h1 className="text-3xl mb-5">Enter your Name</h1>
+        <form onSubmit={handlesubmit} className="flex flex-col gap-5">
+          <input
+            className="px-4 py-2 rounded-lg bg-black outline-none"
+            value={inputval}
+            onChange={e => setInputval(e.target.value)}
+            type="text" placeholder="Enter your name" />
+          <button className="bg-red-600 px-4 py-2 rounded-md" type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  )
 }
--- basic-project/src/app/predection/[name]/page.tsx
+++ basic-project/src/app/predection/[name]/page.tsx
@@ -1,9 +1,41 @@
 
+const getPredictedAge = async (name: string) => {
+    const res = await fetch(`https://api.agify.io?name=${name}`);
+    return res.json();
+};
 
-export default function Name({params}: any) {
+const getPredictedGender = async (name: string) => {
+    const res = await fetch(`https://api.genderize.io?name=${name}`);
+    return res.json();
+};
+
+const getPredictedNationality = async (name: string) => {
+    const res = await fetch(`https://api.nationalize.io?name=${name}`);
+    return res.json();
+};
+interface Params {
+    params: { name: string }
+}
+
+export default async function Name({ params }: Params) {
+    const ageData = getPredictedAge(params.name)
+    const genderData = getPredictedGender(params.name)
+    const countryData = getPredictedNationality(params.name)
+
+    const [age, gender, country] = await Promise.all([
+        ageData, genderData, countryData
+    ])
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1>Welcome buddy, {params.name}</h1>
+            <div className="bg-gray-500 px-4 py-2 rounded-2xl text-3xl">
+                <h1 className="mb-5">Personal Information of {params.name.toUpperCase()}</h1>
+                <div>
+                    <h2>Age: {age.age}</h2>
+                    <h2>Gender: {gender.gender}</h2>
+                    <h2>Country: {country.country[0].country_id}</h2>
+                    {/* <button className="text-white px-4 py-2 rounded-lg bg-red-500 hover:bg-red-800 transition mt-5">Click to Return back Home</button> */}
+                </div>
+            </div>
         </div>
     )
 }
