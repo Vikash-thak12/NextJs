d10f7fbc46c54ae00a8cac33d8d464f4699c9074
Vikash Thakur <thakvikash622@gmail.com>
2024-07-24 14:14:53 +0530

    Setting up the project

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
-      <h1 className="text-white">Hey buddy</h1>
+    <main className="relative bg-black flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1 className="text-3xl text-white">Hey buddy</h1>
+        <Hero />
+      </div>
     </main>
   );
 }
