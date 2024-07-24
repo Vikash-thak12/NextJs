564120149a0fed491031e79963432bf80ae909ed
Vikash Thakur <thakvikash622@gmail.com>
2024-07-24 15:25:11 +0530

    setting up the frontpage

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -3,8 +3,8 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <main className="relative bg-black flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <h1 className="text-3xl text-white">Hey buddy</h1>
+      <div className="max-w-7xl h-[100vh] w-full">
+        {/* <h1 className="text-3xl text-white">Hey buddy</h1> */}
         <Hero />
       </div>
     </main>
--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerator'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
     return (
@@ -9,9 +12,29 @@ const Hero = () => {
                 <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
                 <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue' />
             </div>
-            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+            <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
                 {/* Radial gradient for the container to give a faded look */}
                 <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+            </div>
+            <div className='flex justify-center relative my-20 z-10'>
+                <div className='max-w-[89vh] md:max-w-2xl lg:max-w-[60vw] border border-red-500 flex flex-col items-center justify-center py-5'>
+                    <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Welcome to the webpage</h2>
+                    <TextGenerateEffect
+                        className='text-center text-2xl md:text-5xl lg:text-6xl'
+                        words='Transforming Concepts into Seamless User Experiences'
+                    />
+
+                    <p className='text-center tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+                        Hi, I&apos;m Vikash Thakur, A NextJs Developer from Nepal.
+                    </p>
+
+                    <a href="#about">
+                        <MagicButton 
+                        position='right'
+                        icon={<FaLocationArrow />}
+                        title="Show my work" />
+                    </a>
+                </div>
             </div>
         </div>
     )
