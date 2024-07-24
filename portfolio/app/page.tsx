0c031a75c68ef274ec57c77aafd93c83436310e6
Vikash Thakur <thakvikash622@gmail.com>
2024-07-24 13:31:10 +0530

    Initial commit

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
+      <h1 className="text-white">Hey buddy</h1>
+    </main>
+  );
+}
