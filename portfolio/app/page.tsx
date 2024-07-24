f579ce76c487e92bb2f88504ccf1ae03a279716d
Vikash Thakur <thakvikash622@gmail.com>
2024-07-24 17:23:42 +0530

    Update the code

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,13 +1,16 @@
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import Grid from "@/components/ui/Grid";
+import Grid from "@/components/Grid";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl h-[100vh] w-full">
-        <FloatingNav navItems={[ {name: "Home", link: '/', icon: <FaHome />} ]}/>
+      <div className="max-w-7xl w-full pb-10">
+        <FloatingNav navItems={[
+          { name: "Home", link: '/', icon: <FaHome /> },
+          { name: "about", link: '/about', icon: <FaHome /> }
+        ]} />
         <Hero />
         <Grid />
       </div>
--- portfolio/components/ui/FloatingNavbar.tsx
+++ portfolio/components/ui/FloatingNavbar.tsx
@@ -76,6 +76,7 @@ export const FloatingNav = ({
           <span>Login</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
         </button>
+        
       </motion.div>
     </AnimatePresence>
   );
