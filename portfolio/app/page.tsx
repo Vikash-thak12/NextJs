ad86049609d79d9766056ad307e2ca71cac2f8a8
Vikash Thakur <thakvikash622@gmail.com>
2024-07-25 15:22:49 +0530

    Fixed the card components

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,17 +2,19 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Grid from "@/components/Grid";
 import { FaHome } from "react-icons/fa";
+import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
   return (
     <main className="relative bg-black flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full pb-10">
+      <div className="max-w-7xl pb-10">
         <FloatingNav navItems={[
           { name: "Home", link: '/', icon: <FaHome /> },
           { name: "about", link: '/', icon: <FaHome /> }
         ]} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
