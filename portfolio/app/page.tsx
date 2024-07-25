a20dddaf2a4aabc3b0b460013adc68dc4611a9f6
Vikash Thakur <thakvikash622@gmail.com>
2024-07-25 15:31:55 +0530

    Modified the NavBar

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,17 +1,14 @@
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Grid from "@/components/Grid";
-import { FaHome } from "react-icons/fa";
 import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl pb-10">
-        <FloatingNav navItems={[
-          { name: "Home", link: '/', icon: <FaHome /> },
-          { name: "about", link: '/', icon: <FaHome /> }
-        ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
--- portfolio/components/RecentProjects.tsx
+++ portfolio/components/RecentProjects.tsx
@@ -5,7 +5,7 @@ import { FaLocationArrow } from "react-icons/fa"
 
 const RecentProjects = () => {
     return (
-        <div className="py-20">
+        <div className="py-20" id="projects">
             <h1 className="heading">A Small Collection of {''}
                 <span className="text-purple">
                     Recent Projects
