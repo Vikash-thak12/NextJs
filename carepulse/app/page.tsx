b8ee13d5c7fc7889aa3aa46c35fabcd0eb969753
Vikash Thakur <thakvikash622@gmail.com>
2024-07-28 09:55:36 +0530

    Implemented Shadcn UI

--- carepulse/app/page.tsx
+++ carepulse/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl underline text-white">This is Home page</h1>
+      <Button className="text-white border">Click me</Button>
     </div>
   );
 }
