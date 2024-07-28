27e8d1d1a63c84d33cb3d5b85e8865e637744c99
Vikash Thakur <thakvikash622@gmail.com>
2024-07-28 09:49:46 +0530

    Instll necessary packages

--- carepulse/app/page.tsx
+++ carepulse/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl underline">This is Home page</h1>
+      <h1 className="text-3xl underline text-white">This is Home page</h1>
     </div>
   );
 }
