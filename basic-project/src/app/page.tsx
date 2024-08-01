a98cd5cf3e05d46412b3c20d726c36b4139ef85e
Vikash Thakur <125829628+Vikash-thak12@users.noreply.github.com>
2024-08-01 22:14:47 +0545

    Update page.tsx

--- basic-project/src/app/page.tsx
+++ basic-project/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { FormEvent, useState } from "react"
 import { useRouter } from 'next/navigation'
-// import { useRouter } from "next/router";
+
 
 export default function Home() {
   const [inputval, setInputval] = useState('');
