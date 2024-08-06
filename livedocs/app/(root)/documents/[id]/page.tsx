19edf307ed5c3e30ab504fd2a9c66a7f23aaed61
Vikash Thakur <thakvikash622@gmail.com>
2024-08-06 15:28:33 +0530

    working on editor page

--- livedocs/app/(root)/documents/[id]/page.tsx
+++ livedocs/app/(root)/documents/[id]/page.tsx
@@ -1,11 +1,18 @@
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
 import React from 'react'
 
 const page = () => {
-  return (
-    <div>
-      Documents
-    </div>
-  )
+    return (
+        <div>
+            <Header>
+                <div className='flex w-fit items-center justify-center gap-2'>
+                    <p className='document-title'>This is about Vikash</p>
+                </div>
+            </Header>
+            <Editor />
+        </div>
+    )
 }
 
 export default page
